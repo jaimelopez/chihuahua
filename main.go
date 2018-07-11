7933bd9b227973ff59c5d0bb1e9cef1ae6deebf1
Jaime Lopez <jlopez@magento.com>
2018-07-11 18:18:20 +0200

    Modifies the way in which parameters are processed

--- main.go
+++ main.go
@@ -4,10 +4,10 @@ import (
 	"errors"
 	"flag"
 	"os"
-	"time"
 
 	"github.com/jaimelopez/chihuahua/executor"
 	"github.com/jaimelopez/chihuahua/logger"
+	"github.com/jaimelopez/chihuahua/parameters"
 	"github.com/jaimelopez/chihuahua/printer"
 	"github.com/jaimelopez/chihuahua/storage"
 )
@@ -21,22 +21,13 @@ const appHeader = `
  `
 
 func main() {
-	group := flag.String("group", "", "Group name of metrics to store *")
-	driver := flag.String("storage", "", "Driver to store results (elastic|file) *")
-	destination := flag.String("destination", "", "Storage destination *")
-	duration := flag.Duration("duration", 1*time.Second, "Time to execute benchmarks")
-	threshold := flag.Uint64("threshold", 15, "Threshold percent to determine performance is good enough")
-	save := flag.Bool("save", false, "Results will be saved if results are higher than previous")
-	force := flag.Bool("force", false, "Force to save results even if they are worse")
-	results := flag.Bool("results", false, "Shows results as table")
-	debug := flag.Bool("debug", false, "Shows traces for debugging")
-	flag.Parse()
+	params := parameters.NewFromFlags()
 
-	if *group == "" || *driver == "" || *destination == "" {
+	if *params.Group == "" || *params.Driver == "" || *params.Destination == "" {
 		usage()
 	}
 
-	strg, err := storage.New(*group, *driver, *destination)
+	strg, err := storage.New(*params.Group, *params.Driver, *params.Destination)
 	if err != nil {
 		logger.Error("setting up storage", err)
 	}
@@ -46,20 +37,20 @@ func main() {
 		logger.Error("retrieving latest benchmarks results", err)
 	}
 
-	currentResult, err := executor.Run(*duration, *debug)
+	currentResult, err := executor.Run(*params.Duration, *params.Debug)
 	if err != nil {
 		logger.Error("executing benchmarks", err)
 	} else if len(*currentResult) == 0 {
 		logger.Error("executing benchmarks", errors.New("no test found"))
 	}
 
-	succeed, comparision := executor.Compare(latestResult, currentResult, uint(*threshold))
+	succeed, comparision := executor.Compare(latestResult, currentResult, uint(*params.Threshold))
 
-	if *results {
+	if *params.Results {
 		printer.Print(comparision)
 	}
 
-	if *save && (succeed || *force) {
+	if *params.Save && (succeed || *params.Force) {
 		err = strg.Persist(currentResult)
 		if err != nil {
 			logger.Error("saving results:", err)
@@ -68,7 +59,7 @@ func main() {
 
 	if succeed {
 		exit("Good perfomance dude!", 0)
-	} else if *force {
+	} else if *params.Force {
 		exit("Bad performance but forcing...", 0)
 	}
 
---
+++ parameters/parameters.go
@@ -0,0 +1,38 @@
+package parameters
+
+import (
+	"flag"
+	"time"
+)
+
+// Params defines behaviour to run the app
+type Parameters struct {
+	Group       *string
+	Driver      *string
+	Destination *string
+	Duration    *time.Duration
+	Threshold   *uint64
+	Save        *bool
+	Force       *bool
+	Results     *bool
+	Debug       *bool
+}
+
+// NewFromFlags instantiate a new params struct from flags
+func NewFromFlags() *Parameters {
+	params := &Parameters{
+		Group:       flag.String("group", "", "Group name of metrics to store *"),
+		Driver:      flag.String("storage", "", "Driver to store results (elastic|file) *"),
+		Destination: flag.String("destination", "", "Storage destination *"),
+		Duration:    flag.Duration("duration", 1*time.Second, "Time to execute benchmarks"),
+		Threshold:   flag.Uint64("threshold", 15, "Threshold percent to determine performance is good enough"),
+		Save:        flag.Bool("save", false, "Results will be saved if results are higher than previous"),
+		Force:       flag.Bool("force", false, "Force to save results even if they are worse"),
+		Results:     flag.Bool("results", false, "Shows results as table"),
+		Debug:       flag.Bool("debug", false, "Shows traces for debugging"),
+	}
+
+	flag.Parse()
+
+	return params
+}
